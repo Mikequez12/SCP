e4a17f10922007308730d85bdb3239434f537b82
Mike <142720633+Mikequez12@users.noreply.github.com>
2024-09-04 20:43:03 +0200

    Update scp-load.js

--- NicoCore/scp-load.js
+++ NicoCore/scp-load.js
@@ -1,21 +1,23 @@
 var main_json_file = "https://raw.githubusercontent.com/scp-data/scp-api/main/docs/data/scp/items/content_archived.json"
 
-fetch(main_json_file)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Error al cargar el archivo JSON');
-        }
-        return response.json(); // Convertir a JSON
-    })
-    .then(data => {
-        // Aquí puedes usar los datos cargados
-        const outputDiv = document.getElementById('output');
-        outputDiv.innerHTML = `
-            <p>Nombre: ${data.name}</p>
-            <p>Edad: ${data.age}</p>
-            <p>Ciudad: ${data.city}</p>
-        `;
-    })
-    .catch(error => {
-        console.error('Hubo un problema con la solicitud Fetch:', error);
-    });
+document.addEventListener('DOMContentLoaded', (main_event) => {
+    fetch(main_json_file)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al cargar el archivo JSON');
+            }
+            return response.json(); // Convertir a JSON
+        })
+        .then(data => {
+            // Aquí puedes usar los datos cargados
+            const outputDiv = document.getElementById('output');
+            outputDiv.innerHTML = `
+                <p>Nombre: ${data.name}</p>
+                <p>Edad: ${data.age}</p>
+                <p>Ciudad: ${data.city}</p>
+            `;
+        })
+        .catch(error => {
+            console.error('Hubo un problema con la solicitud Fetch:', error);
+        });
+});
